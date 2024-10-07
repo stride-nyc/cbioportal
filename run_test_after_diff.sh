6847c678fc266f35cbd53ff03cfc506dd3bafe93
Stephen Meriwether <stephen@merimerimeri.com>
2024-10-07 15:31:36 -0400

    Conductor setup

---
+++ run_test_after_diff.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the list of changed, added, or untracked test files in the current uncommitted state
+changed_tests=$(git diff --name-only --diff-filter=AM | grep 'src/test/java/.*Test\.java')
+untracked_tests=$(git ls-files --others --exclude-standard | grep 'src/test/java/.*Test\.java')
+
+# Combine the lists and format for Maven
+all_tests=$(echo -e "$changed_tests\n$untracked_tests" | sed 's/src\/test\/java\///;s/\//./g;s/\.java//' | paste -sd, -)
+
+# Check if there are any test files to run
+if [ -n "$all_tests" ]; then
+  # Run the tests
+ mvn test -q -Dtest="$all_tests"
+else
+  echo "No test files have been changed, added, or are untracked."
+fi
